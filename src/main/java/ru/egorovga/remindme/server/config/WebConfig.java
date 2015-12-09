c30e498da98f7c292014be5b904be49cfafe3ece
Gennadij Egorov <egorov_g_a@mail.ru>
2015-12-09 16:12:11 +0300

    Configure base project and create HelloWorld controller.

---
+++ src/main/java/ru/egorovga/remindme/server/config/WebConfig.java
@@ -0,0 +1,13 @@
+package ru.egorovga.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.egorovga.remindme.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
