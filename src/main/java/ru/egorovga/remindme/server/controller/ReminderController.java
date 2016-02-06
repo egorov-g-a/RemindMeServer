05c77d75f449f224700c0b95d29e6912084b308f
Gennadij Egorov <egorov_g_a@mail.ru>
2016-02-06 15:28:59 +0300

    Improved ReminderController. Included parsing ours remind objects to JSON.

--- src/main/java/ru/egorovga/remindme/server/controller/ReminderController.java
+++ src/main/java/ru/egorovga/remindme/server/controller/ReminderController.java
@@ -1,19 +1,29 @@
 package ru.egorovga.remindme.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.egorovga.remindme.server.entity.Remind;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap model){
+    public Remind getReminder(){
+        return createmMockRemind();
+    }
 
-        return "My reminder";
+    private Remind createmMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setTitle("My first remind title");
+        remind.setRemindDate(new Date());
+
+        return remind;
     }
 }
